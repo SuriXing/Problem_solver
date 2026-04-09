6b3f92582112c0e75b39e08a11817ffd72e2bbdc
SuriXing <SuriXing@users.noreply.github.com>
2026-04-09 09:05:43 +0000

    deploy: e45ce1be647251796c80670e8f6a7c010dba53ee

--- assets/mentor-table-659144ac.js
+++
@@ -1,2 +0,0 @@
-import{ac as o,j as e,R as r,ad as a,ab as s}from"./index-433d4158.js";const t=document.getElementById("root");t&&o.createRoot(t).render(e.jsx(r.StrictMode,{children:e.jsx(a,{children:e.jsx(s,{standalone:!0})})}));
-//# sourceMappingURL=mentor-table-659144ac.js.map
